54ed2699c4ea1ac5ea9e1c9feaeed5bb90ddab46
Takayoshi Makabe <makabe.takayoshi1117@gmail.com>
2020-01-21 00:50:04 +0900

    training

---
+++ other/apg4b/ex010.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  int ca=0;
+  int cb=0;
+  cout << "A:";
+  while (ca < A) {
+      cout << "]";
+      ca++;
+  }
+  cout << endl;
+  cout << "B:";
+  while (cb < B) {
+      cout << "]";
+      cb++;
+  }
+  cout << endl;
+}
---
+++ other/apg4b/ex011.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, A;
+    cin >> N >> A;
+    for (int i=0; i<N; i++) {
+        string k;
+        int n;
+        cin >> k >> n;
+        if (k=="/" && n==0) {
+            cout << "error" << endl;
+            break;
+        }
+        if (k=="+") {
+            A += n;
+        }
+        else if (k=="-") {
+            A -= n;
+        }
+        else if (k=="*") {
+            A *= n;
+        }
+        else {
+            A /= n;
+        }
+        cout << i+1 << ":" << A << endl;
+    }
+}
