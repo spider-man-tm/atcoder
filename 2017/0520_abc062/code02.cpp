90ba84ebe954340a7eb9490ce8e44af1cfca5c65
Takayoshi Makabe <makabe.takayoshi1117@gmail.com>
2020-01-29 17:39:57 +0900

    training

--- 2017/0520_abc062/code02.cpp
+++ 2017/0520_abc062/code02.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main() {
     int h, w;
     cin >> h >> w;
-    
+    string bar(w+2, '#'), tmp;
+    cout << bar << endl;
+    for (int i=0; i<h; i++) {
+        cin >> tmp;
+        cout << '#' << tmp << '#' << endl;
+    }
+    cout << bar << endl;
 }
